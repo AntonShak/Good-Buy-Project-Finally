8f195a5ba3a288ce9eaa9190b5105f229bc608d7
Anton Shakov <annawinnern1@gmail.com>
2024-05-22 19:29:06 +0300

    added integration annotation IT

---
+++ src/test/java/com/shakov/goodbuyproject/integrartion/annotation/IT.java
@@ -0,0 +1,19 @@
+package com.shakov.goodbuyproject.integrartion.annotation;
+
+import com.shakov.goodbuyproject.TestApplicationRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Transactional
+@SpringBootTest(classes = TestApplicationRunner.class)
+@ActiveProfiles("test")
+public @interface IT {
+}
