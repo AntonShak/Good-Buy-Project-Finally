43e0d9f22e2f0475f05a6d70549b1cfa1d769656
Anton Shakov <annawinnern1@gmail.com>
2024-05-22 19:34:25 +0300

    added dto for User

---
+++ src/main/java/com/shakov/goodbuyproject/dto/UserCreateEditDto.java
@@ -0,0 +1,19 @@
+package com.shakov.goodbuyproject.dto;
+
+import com.shakov.goodbuyproject.database.entity.Role;
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
+@Builder
+public class UserCreateEditDto {
+
+    String username;
+    LocalDate birthDate;
+    String firstname;
+    String lastname;
+    String phone;
+    Role role;
+}
