81b06c3dc2c547eeea8dcf1f670662ff840b01ff
Anton Shakov <annawinnern1@gmail.com>
2024-05-22 20:32:31 +0300

    tested userService bean

--- src/main/java/com/shakov/goodbuyproject/GoodBuyProjectApplication.java
+++ src/main/java/com/shakov/goodbuyproject/GoodBuyProjectApplication.java
@@ -1,13 +1,27 @@
 package com.shakov.goodbuyproject;
 
+import com.shakov.goodbuyproject.dto.MarketplaceReadDto;
+import com.shakov.goodbuyproject.dto.UserReadDto;
+import com.shakov.goodbuyproject.service.MarketplaceService;
+import com.shakov.goodbuyproject.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class GoodBuyProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GoodBuyProjectApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(GoodBuyProjectApplication.class, args);
+
+        UserService userService = context.getBean(UserService.class);
+
+        List<UserReadDto> all = userService.findAll();
+
+        System.out.println(all);
     }
 
 }
