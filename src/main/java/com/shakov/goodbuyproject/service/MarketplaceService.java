e5fba3b070ddd91135952dabe8c75c6177d8b685
Anton Shakov <annawinnern1@gmail.com>
2024-05-22 18:42:19 +0300

    added method findById for MarketplaceService

--- src/main/java/com/shakov/goodbuyproject/service/MarketplaceService.java
+++ src/main/java/com/shakov/goodbuyproject/service/MarketplaceService.java
@@ -1,14 +1,23 @@
 package com.shakov.goodbuyproject.service;
 
 import com.shakov.goodbuyproject.database.repository.MarketplaceRepository;
+import com.shakov.goodbuyproject.dto.MarketplaceReadDto;
+import com.shakov.goodbuyproject.mapper.MarkeplaceReadDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MarketplaceService {
 
-    private final MarketplaceRepository marketplaceRepository;
+    private final MarkeplaceReadDtoMapper markeplaceReadDtoMapper;
+    private final  MarketplaceRepository marketplaceRepository;
 
 
+    public Optional<MarketplaceReadDto> findById(Long id) {
+        return marketplaceRepository.findById(id)
+                .map(markeplaceReadDtoMapper::map);
+    }
 }
