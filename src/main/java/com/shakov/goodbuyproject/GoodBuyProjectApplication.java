2947c295a72e391a4c96f71436afc78805ba74d3
Anton Shakov <annawinnern1@gmail.com>
2024-05-22 17:03:05 +0300

    Created new project

---
+++ src/main/java/com/shakov/goodbuyproject/GoodBuyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shakov.goodbuyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBuyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodBuyProjectApplication.class, args);
+    }
+
+}
