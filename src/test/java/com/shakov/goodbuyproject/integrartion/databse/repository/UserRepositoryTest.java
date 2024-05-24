c40556f3e4a268ff1a6eac550c07cee14b10d7bd
Anton Shakov <annawinnern1@gmail.com>
2024-05-24 14:50:38 +0300

    added UserRepositoryTest fot auditing

---
+++ src/test/java/com/shakov/goodbuyproject/integrartion/databse/repository/UserRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.shakov.goodbuyproject.integrartion.databse.repository;
+
+import com.shakov.goodbuyproject.database.entity.User;
+import com.shakov.goodbuyproject.database.repository.UserRepository;
+import com.shakov.goodbuyproject.integrartion.annotation.IT;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.Instant;
+
+
+
+import static org.assertj.core.api.Assertions.*;
+
+@IT
+@RequiredArgsConstructor
+public class UserRepositoryTest {
+
+    private final UserRepository userRepository;
+    private final EntityManager entityManager;
+
+    @Test
+    void checkAuditing() {
+
+        User user = userRepository.findById(1L).get();
+        user.setBirthDate(user.getBirthDate().plusYears(1L));
+        entityManager.flush();
+
+        Instant actualDate = user.getModifiedAt();
+        String actualBy = user.getModifiedBy();
+
+
+        assertThat(actualDate).isNotNull();
+        assertThat(actualBy).isEqualTo("Shakov");
+
+
+
+    }
+
+
+}
