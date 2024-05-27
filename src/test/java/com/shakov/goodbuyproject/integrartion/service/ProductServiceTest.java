37048662eafbee617c945d495c0d7961452f775b
Anton Shakov <annawinnern1@gmail.com>
2024-05-28 00:20:55 +0300

    created int test on ProductService findById()

---
+++ src/test/java/com/shakov/goodbuyproject/integrartion/service/ProductServiceTest.java
@@ -0,0 +1,32 @@
+package com.shakov.goodbuyproject.integrartion.service;
+
+import com.shakov.goodbuyproject.dto.ProductReadDto;
+import com.shakov.goodbuyproject.integrartion.IntegrationTestBase;
+import com.shakov.goodbuyproject.integrartion.annotation.IT;
+import com.shakov.goodbuyproject.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@IT
+@RequiredArgsConstructor
+public class ProductServiceTest extends IntegrationTestBase {
+
+    public static final Long COAT_ID = 2L;
+
+    private final ProductService productService;
+
+    @Test
+    void findById() {
+
+        Optional<ProductReadDto> actualResult = productService.findById(COAT_ID);
+
+        assertTrue(actualResult.isPresent());
+        actualResult.ifPresent(product ->
+                assertEquals("185777425", actualResult.get().getVendorCode()));
+
+    }
+}
