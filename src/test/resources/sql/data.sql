0735951fdbce2e944cad7f8d430e36affae17ebb
Anton Shakov <annawinnern1@gmail.com>
2024-05-27 22:26:37 +0300

    changed seq_id strategy

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -2,10 +2,12 @@ INSERT INTO marketplace (id, name)
 VALUES (1, 'Wildberries'),
        (2, 'Ozon'),
        (3, 'Onliner');
+SELECT SETVAL('marketplace_id_seq', (SELECT MAX(id) FROM marketplace));
 
 INSERT INTO users (id, username, birth_date, firstname, lastname, phone, role)
 VALUES (1, 'shakov@gmail.com', '01-19-1992', 'Anton', 'Shakov', '+375291475900', 'ADMIN'),
        (2, 'shakova@gmail.com', '02-22-1992', 'Tatsiana', 'Shakova', '+375291921614', 'USER');
+SELECT SETVAL('users_id_seq', (SELECT MAX(id) FROM users));
 
 INSERT INTO product (id, vendor_code, user_id, marketplace_id, name, description, price, discount)
 VALUES (1, '197046833', (SELECT id FROM users WHERE firstname = 'Tatsiana'),
@@ -23,6 +25,8 @@ VALUES (1, '197046833', (SELECT id FROM users WHERE firstname = 'Tatsiana'),
        (5, 'KIT FB0319', (SELECT id FROM users WHERE firstname = 'Anton'),
         (SELECT id FROM marketplace WHERE name = 'Ozon'), 'Робот',
         'Робот для мытья окон Даджет dBot W200', 369, 11);
+SELECT SETVAL('marketplace_id_seq', (SELECT MAX(id) FROM product));
+
 
 INSERT INTO users_marketplace(user_id, marketplace_id)
 VALUES ((SELECT id FROM users WHERE username = 'shakov@gmail.com'), (SELECT id FROM marketplace WHERE name = 'Wildberries')),
@@ -30,6 +34,7 @@ VALUES ((SELECT id FROM users WHERE username = 'shakov@gmail.com'), (SELECT id F
        ((SELECT id FROM users WHERE username = 'shakov@gmail.com'), (SELECT id FROM marketplace WHERE name = 'Onliner')),
        ((SELECT id FROM users WHERE username = 'shakova@gmail.com'), (SELECT id FROM marketplace WHERE name = 'Wildberries')),
        ((SELECT id FROM users WHERE username = 'shakova@gmail.com'), (SELECT id FROM marketplace WHERE name = 'Ozon'));
+SELECT SETVAL('marketplace_id_seq', (SELECT MAX(id) FROM users_marketplace));
 
 
 
