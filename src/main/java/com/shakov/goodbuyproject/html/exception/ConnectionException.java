0494a91165b6bc82e138f2d305e219454c578ca7
Anton Shakov <annawinnern1@gmail.com>
2024-05-30 18:50:27 +0300

    added ConnectionException for jsoup

---
+++ src/main/java/com/shakov/goodbuyproject/html/exception/ConnectionException.java
@@ -0,0 +1,9 @@
+package com.shakov.goodbuyproject.html.exception;
+
+public class ConnectionException extends RuntimeException{
+
+    public ConnectionException(String message) {
+        super(message);
+    }
+
+}
