83e895552134392241f7212b47e9bce8002d8393
Anton Shakov <annawinnern1@gmail.com>
2024-06-10 03:25:41 +0300

    fixed Wildberries create product functionality

--- src/main/java/com/shakov/goodbuyproject/GoodBuyProjectApplication.java
+++ src/main/java/com/shakov/goodbuyproject/GoodBuyProjectApplication.java
@@ -3,12 +3,11 @@ package com.shakov.goodbuyproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Duration;
-
 @SpringBootApplication
 public class GoodBuyProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodBuyProjectApplication.class, args);
+
     }
 }
